b9b7fb4c1f4d9cbf20ea69fd8d47b36a368f1a66
nairut <turian@hotmail.com>
2023-09-29 15:35:28 -0300

    Update index.jsx

--- src/components/others/ArchiveType/index.jsx
+++ src/components/others/ArchiveType/index.jsx
@@ -1,116 +1,190 @@
-import { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { languages, typeContent} from '../../../constants'
+// import { MultiSelect } from 'react-multi-select-component';
 import './style.css'
 
-const Archive = ({data, updateFieldHandler}) => {
-  const [numArchives, setNumArchives] = useState(0)
-  const input = useRef()
-  const file1 = useRef()
-  const file2 = useRef()
-  const file3 = useRef()
-  const file4 = useRef()
-  const file5 = useRef()
-  const file6 = useRef()
-  const refsArray = [file1, file2, file3, file4, file5, file6]
-  const handleFileChange = (e) => {
-    const count = e.target.files.length;
-    const files = e.target.files
-    addFiles(files, e)
-    let arrayFile = []
-    for (let index = 0; index < files.length; index++) {
-      const element = files[index];
-      arrayFile.push(element)
-    }
-    setNumArchives(count);
-  }
-  const addFiles = (files, e) => {
-    
-    
-    for (let index = 0; index < files.length; index++) {
-      let data = new DataTransfer()
-      const element = files[index];
+const Item = ({name, handleClick}) => {
+  const [isChecked, setIsChecked] = useState(false);
 
-      data.items.add(element)
-
-      // console.log(data.items.length, data.files[0]);
-
-      const input = refsArray[index]
-
-      input.current.files = data.files
-
-      
-    }
-
-    limparInputFile();
-    // files.forEach((file, index) => {
-    //   const inputRef = refsArray[index]
-    //   inputRef.current.files = [file]
-    // } )
-  }
-  const handleDrop = (e) => {
-    e.preventDefault()
-    if (e.dataTransfer.files.length > 0) {
-      addFiles(e.dataTransfer.files)
-      input.current.files = e.dataTransfer.files; 
-      setNumArchives(e.dataTransfer.files.length)
-    }
-  }
-  const limparInputFile = () => {
-    input.current.value = ""
+  const handleItemClick = () => {
+    setIsChecked(!isChecked);
   };
+
+  useEffect(() => {
+    handleClick()
+  },[isChecked])
   return (
-    <div>
-      <input type='hidden' name='form-name' value="Quotation" />
-      <div className='input-steps-content'>
-      <div className="input display-none">
-            <label htmlFor="">Do que se trata o seu conteudo</label> <br />
-            <input type="text" name='conteudo' value={data.typeArchive || ""} placeholder='Tipo de conteudo: pdf, img ...' />
-        </div>
-        <div className="input display-none">
-            <label htmlFor="">Idioma da origem</label> <br />
-            <input type="text" name='origem' value={data.origin || ""} placeholder='Qual é o idioma do documento?' />
-        </div>
-        <div className="input display-none">
-            <label htmlFor="">Idioma para tradução</label> <br />
-            <input type="text" name='tradução' value={data.translation || ""} placeholder='Idimo para qual você quer traduzir' />
-        </div>
-        <div className="input display-none">
-            <input type="text" name='name' placeholder='Tipo de conteudo: pdf, img ...' value={data.name || ""} required/>
-        </div>
-        <div className="input display-none">
-            <input type="text" name='office' placeholder='Qual é o idioma do documento?' value={data.office || ""} />
-        </div>
-        <div className="input display-none">
-            <input type="email" name='email' placeholder='Idimo para qual você quer traduzir' value={data.email || ""} required/>
-        </div>
-      <div className="input file" onDragOver={(e) => e.preventDefault()} onDrop={handleDrop}>
-            <p>Adicione seus arquivos:</p>
-            <br />
-            <label htmlFor='file'>Adicione seus arquivos <br />
-            Adicionados: {numArchives} <br />
-            </label>
-            
-            <input ref={input} onDragOver={(e) => e.preventDefault()} type="file" id='file' name='archive8' onDrop={handleDrop} onChange={handleFileChange} multiple/>
-        </div>
-        <div className="input display-none">
-          <input ref={file1} type="file" name='archive1' multiple/>
-          <input ref={file2} type="file" name='archive2' multiple/>
-          <input ref={file3} type="file" name='archive3' multiple/>
-          <input ref={file4} type="file" name='archive4' multiple/>
-          <input ref={file5} type="file" name='archive5' multiple/>
-          <input ref={file6} type="file" name='archive6' multiple/>
-        </div>
-      {/* <div className="input file">
-            <label htmlFor="">Adicione seus arquivos</label> <br />
-            <input type="file" name='archive2'/>
-        </div>
-      <div className="input file">
-            <label htmlFor="">Adicione seus arquivos</label> <br />
-            <input type="file" name='archive3'/>
-        </div> */}
-    </div>
-    </div>
-    
+    <li className={`item ${isChecked ? 'checked' : ''}`} onClick={handleItemClick} >
+        <span className="checkbox">
+            <i className="fa-solid fa-check check-icon"></i>
+        </span>
+        <span className="item-text">{name}</span>
+    </li>
   )
 }
 
-export default Archive
+const ArchiveType = ({data, updateFieldHandler, inputs}) => {
+    // const [selected, setSelected] = useState([]);
+    const [openFirst, setOpenFirst] = useState(false)
+    const [languagens, setLanguages] = useState()
+    const selectRef = useRef(null)
+    // const [selectedOrigin, setSelectedOrigin] = useState([]);
+    // const [selectedArchiveType, setSelectedArchiveType] = useState([])
+
+    // const handleChangeOrigin = (option) => {
+    //   console.log(option);
+    //   if (selectedOrigin[0]) {
+    //     setSelectedOrigin(prev => {
+    //       if (option[1]) {
+    //         updateFieldHandler("origin", option[1].label)
+    //         return [option[1]]  
+    //       } else {
+    //         updateFieldHandler("origin", [])
+    //         return []
+    //       }
+    //     })
+    //   } else {
+    //     setSelectedOrigin(prev => {
+    //       updateFieldHandler("origin", option[0].label || "")
+    //       return option
+    //     })
+    //   }
+    // }
+    // const handleChangeTranslation = (options) => {
+    //   let string = []
+    //   options.forEach(element => {
+    //     string.push(element.label)
+    //   });
+    //   setSelected(prev => {
+    //     updateFieldHandler("translation", string.join(", "))
+    //     return options
+    //   })
+    // }
+    // const handleChangeContent = (option) => {
+    //   console.log(option);
+    //   if (selectedArchiveType[0]) {
+    //     setSelectedArchiveType(prev => {
+    //       if (option[1]) {
+    //         updateFieldHandler("typeArchive", option[1].label)
+    //         return [option[1]]  
+    //       } else {
+    //         updateFieldHandler("typeArchive", [])
+    //         return []
+    //       }
+    //     })
+    //   } else {
+    //     setSelectedArchiveType(prev => {
+    //       updateFieldHandler("typeArchive", option[0].label || "")
+    //       return option
+    //     })
+    //   }
+    // }
+    // if (selectRef.current && !selectRef.current.contains(e.target)) {
+    // }
+    const handleClick = () => {
+      setOpenFirst(prev => !prev)
+    }
+
+    const handleGetValue = () => {
+      const elements = document.getElementById('list').querySelectorAll('.checked');
+      let itens = []
+      elements.forEach((element) => {
+        itens.push(element.textContent)
+      });
+      updateFieldHandler("translation", itens.join(", "))
+      if (elements.length > 4) {
+        const newLayout = itens.slice(0, 4)
+        setLanguages(`${newLayout.join(", ")}...`)
+      } else {
+
+        setLanguages(itens.join(", "))
+      }
+    }
+    useEffect(() => {
+      const handleClickOutside = (e) => {
+        console.log(selectRef);
+        if (selectRef.current && !selectRef.current.contains(e.target)) {
+          setOpenFirst(false);
+        }
+      }
+
+      if (openFirst) {
+        document.addEventListener('click', handleClickOutside)
+      } else {
+        document.removeEventListener('click', handleClickOutside)
+      }
+
+      return () => {
+        document.removeEventListener('click', handleClickOutside)
+      }
+    },[openFirst])
+  return (
+    <div className='input-steps-content'>
+        <div className="input">
+            <label htmlFor="content">{inputs[0]}</label> <br />
+            <select name="content" id="content" onChange={e => updateFieldHandler("typeArchive", e.target.value)}>
+            {typeContent.map((item, index) => (
+              <option key={index} value={item.value}>{item.label}</option>
+            ))}
+            </select>
+            {/* <MultiSelect
+              options={typeContent}
+              value={selectedArchiveType}
+              onChange={handleChangeContent}
+              hasSelectAll={false}
+              closeOnChangedValue={true}
+            /> */}
+            
+        </div>
+        <div className="input">
+            <label htmlFor="languages">{inputs[1]}</label> <br />
+            <select name="languages" id="languages" onChange={e => updateFieldHandler("origin", e.target.value)}>
+            {languages.map((item, index) => (
+              <option key={index} value={item.value}>{item.label}</option>
+            ))}
+            </select>
+            
+            {/* <MultiSelect
+              options={languages}
+              value={selectedOrigin}
+              onChange={handleChangeOrigin}
+              hasSelectAll={false}
+              closeOnChangedValue={true}
+            /> */}
+            {/* <select value={data.origin} name="origem" onChange={(e) => updateFieldHandler("origin", e.target.value)} id="">
+              {languages.map((item, index) => (
+                <option value={item.value}>{item.label}</option>
+              ))}
+
+            </select> */}
+        </div>
+        <div className="input" ref={selectRef}>
+          
+            <label>{inputs[2]}</label> <br />
+            <div  className={`select select-btn ${openFirst && 'open'}`} onClick={handleClick}>
+                <span className="btn-text">{languagens || "Selecione"}</span>
+            </div>
+            <ul className="list-items" id='list'>
+                {languages.map((item, index) => (
+                  <Item key={index} name={item.label} handleClick={handleGetValue} />
+                ))}
+
+            </ul>
+            {/* <MultiSelect
+              options={languages}
+              value={selected}
+              onChange={handleChangeTranslation}
+              hasSelectAll={false}
+            /> */}
+            {/* <select value={""} name="tradução" id="traducao">
+              {languages.map((item, index) => (
+                <option value={item.value}>{item.label}</option>
+              ))}
+
+            </select> */}
+        </div>
+    </div>
+  )
+}
+
+export default ArchiveType
